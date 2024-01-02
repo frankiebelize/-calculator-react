978d2e467f482b8ec9294b2075fd8f43b65ac979
frankiebelize <66229510+frankiebelize@users.noreply.github.com>
2024-01-02 01:07:17 -0600

    completed project

--- calculator/src/main.jsx
+++ calculator/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>My Calculator</h1>
+    <h1 className='title'>My Calculator</h1>
     <App />
   </React.StrictMode>,
 )
